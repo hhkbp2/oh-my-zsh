3868005da4b647275bef71ea58843f9803143b69
Dylan.Wen <hhkbp2@gmail.com>
2023-06-15 21:24:15 +0800

    update for endeavouros

--- custom/os.zsh
+++ custom/os.zsh
@@ -20,7 +20,7 @@ if [[ "${os_type}" = "Linux" ]]; then
                 export PATH="${MY_DIR}/local/opt/texlive/bin/x86_64-linux:$PATH"
             fi
             ;;
-        arch)
+        arch | endeavouros)
             http_proxy_on() {
                 export http_proxy=http://192.168.229.1:7890
                 export https_proxy=http://192.168.229.1:7890
--- lib/grep.zsh
+++ lib/grep.zsh
@@ -3,6 +3,6 @@
 # Examples: http://rubyurl.com/ZXv
 #
 #export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'
+export GREP_COLOR='mt=1;32'
 
 alias grep='grep --color=auto'
