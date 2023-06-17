b2f6459df7cb610d7dbd2b267d2f7ae1fcbb3187
Dylan.Wen <hhkbp2@gmail.com>
2023-06-17 15:08:05 +0800

    update paths

--- custom/bashrc.zsh
+++ custom/bashrc.zsh
@@ -26,33 +26,33 @@ export EDITOR=vim
 alias em='env LC_CTYPE=zh_CN.UTF-8 emacs -nw --debug-init'
 
 # set my local directory
-MY_DIR=${HOME}/dev
-MY_LOCAL_DIR=${MY_DIR}/local
+DEV_DIR=${HOME}/dev
+DEV_LOCAL_DIR=${DEV_DIR}/local
 
 # make elem in path uniq
 typeset -U path
 
 # add my local bin
-if [ -d "${MY_LOCAL_DIR}/bin" ]; then
-    PATH=${MY_LOCAL_DIR}/bin:$PATH
+if [ -d "${DEV_LOCAL_DIR}/bin" ]; then
+    PATH=${DEV_LOCAL_DIR}/bin:$PATH
 fi
 
 # source helper functions
-func_file=${MY_LOCAL_DIR}/bin/helper_functions.sh
+func_file=${DEV_LOCAL_DIR}/bin/helper_functions.sh
 if [ -f "${func_file}" ]; then
     source "${func_file}"
 fi
 
 ## C/C++
 # set environment virables to add include path for gcc/g++
-if [ -d "${MY_LOCAL_DIR}"/include ]; then
-    export C_INCLUDE_PATH=${MY_LOCAL_DIR}/include
+if [ -d "${DEV_LOCAL_DIR}"/include ]; then
+    export C_INCLUDE_PATH=${DEV_LOCAL_DIR}/include
     export CPLUS_INCLUDE_PATH=${C_INCLUDE_PATH}
 fi
 
 # append include path for apue/unp header
-APUE_ROOT=${MY_DIR}/pro/topic/linux/apue/src
-UNP_ROOT=${MY_DIR}/pro/topic/network/unp/src
+APUE_ROOT=${DEV_DIR}/pro/topic/linux/apue/src
+UNP_ROOT=${DEV_DIR}/pro/topic/network/unp/src
 if [ -d "${APUE_ROOT}" ]; then
     export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${APUE_ROOT}/include:${UNP_ROOT}
     export CPLUS_INCLUDE_PATH=${C_INCLUDE_PATH}
@@ -78,20 +78,22 @@ if [ -z "${JAVA_HOME}" ] && [ -d "/usr/lib/jvm/java-7-oracle" ]; then
     fi
 fi
 # set environment for TIJ4
-TIJ4_SRC_HOME=${MY_DIR}/pro/lang/java/tij4/TIJ4-code
+TIJ4_SRC_HOME=${DEV_DIR}/pro/lang/java/tij4/TIJ4-code
 if [ -d "${TIJ4_SRC_HOME}" ]; then
     export TIJ4_SRC_HOME=${TIJ4_SRC_HOME}
     export CLASSPATH=$CLASSPATH:$TIJ4_SRC_HOME
 fi
 
 ## Go
-if [[ -d "${MY_DIR}/local/opt/go" ]]; then
-    export GOROOT=${MY_DIR}/local/opt/go
+if [[ -d "${DEV_LOCAL_DIR}/opt/go" ]]; then
+    export GOROOT=${DEV_LOCAL_DIR}/opt/go
     export PATH="$GOROOT/bin:$PATH"
 fi
 # Go module
-if [[ -d "${HOME}/go" ]]; then
-    export PATH="${HOME}/go/bin:$PATH"
+if [[ -d "${DEV_DIR}/go" ]]; then
+    # customize GOPATH
+    export GOPATH=${DEV_DIR}/go
+    export PATH="${GOPATH}/bin:$PATH"
 fi
 
 ## Rust
@@ -99,17 +101,17 @@ if [[ -d "${HOME}/.cargo" ]]; then
     export PATH="${HOME}/.cargo/bin:$PATH"
 fi
 # Rust source code
-if [[ -d "${MY_DIR}/pro/code/rustc-nightly/src" ]]; then
-    export RUST_SRC_PATH=${MY_DIR}/pro/code/rustc-nightly/src
+if [[ -d "${DEV_DIR}/pro/code/rustc-nightly/src" ]]; then
+    export RUST_SRC_PATH=${DEV_DIR}/pro/code/rustc-nightly/src
 fi
 
 ## Python
 # add module path for python
-if [ -d "${MY_DIR}/local/lib/python2.7/site-packages/" ]; then
+if [ -d "${DEV_LOCAL_DIR}/lib/python2.7/site-packages/" ]; then
     if [ -z "$PYTHONPATH" ]; then
-        export PYTHONPATH=${MY_DIR}/local/lib/python2.7/site-packages/
+        export PYTHONPATH=${DEV_LOCAL_DIR}/lib/python2.7/site-packages/
     else
-        export PYTHONPATH=${MY_DIR}/local/lib/python2.7/site-packages/:$PYTHONPATH
+        export PYTHONPATH=${DEV_LOCAL_DIR}/lib/python2.7/site-packages/:$PYTHONPATH
     fi
 fi
 # python startup script
@@ -137,14 +139,14 @@ if command -v joxa &>/dev/null; then
 fi
 
 ## Elixir
-if [[ -d "${MY_DIR}/local/opt/elixir" ]]; then
-    export ELIXIR_HOME="${MY_DIR}/local/opt/elixir"
+if [[ -d "${DEV_LOCAL_DIR}/opt/elixir" ]]; then
+    export ELIXIR_HOME="${DEV_LOCAL_DIR}/opt/elixir"
     export PATH=$ELIXIR_HOME/bin:$PATH
 fi
 
 # Spacemacs
-if [[ -d "${MY_DIR}/pro/rep/spacemacs" ]]; then
-    alias spacemacs="HOME=${MY_DIR}/pro/rep/spacemacs emacs"
+if [[ -d "${DEV_DIR}/pro/rep/spacemacs" ]]; then
+    alias spacemacs="HOME=${DEV_DIR}/pro/rep/spacemacs emacs"
 fi
 
 ## HG
--- custom/functional.zsh
+++ custom/functional.zsh
@@ -10,14 +10,14 @@ is_option_set() {
 
 switch_locale_zh() {
     export LANG=zh_CN.UTF-8
-    export LANGUAGE=zh_CN:zh:en_US:en
+    export LANGUAGE=zh_CN.UTF-8
     export LC_ALL=zh_CN.UTF-8
     export LC_CTYPE=zh_CN.UTF-8
 }
 
 switch_locale_en() {
     export LANG=en_US.UTF-8
-    export LANGUAGE=en_US:en:zh_CN:zh
+    export LANGUAGE=en_US.UTF-8
     export LC_ALL=en_US.UTF-8
     export LC_CTYPE=en_US.UTF-8
 }
